da5ce83b4c7f3a5576d8409ee97f59b40dd84e66
zdodev <iver0945@gmail.com>
2021-04-06 13:18:18 +0900

    ✨: Create an Enum class.

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+from enum import Enum
 from fastapi import FastAPI
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 
 app = FastAPI()
 
@@ -21,3 +29,14 @@ async def read_user_me():
 @app.get("/users/{user_id}")
 async def read_user(user_id: str):
     return {"user_id": user_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
